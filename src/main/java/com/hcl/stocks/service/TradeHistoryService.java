23378779f37c9d479468da4aee42bb362ca7a6ba
user1 <user1@LP-71VDTS1.HCLT.CORP.HCL.IN>
2019-05-07 14:27:24 +0530

    adding codes for confirm order

--- src/main/java/com/hcl/stocks/service/TradeHistoryService.java
+++ src/main/java/com/hcl/stocks/service/TradeHistoryService.java
@@ -2,7 +2,9 @@ package com.hcl.stocks.service;
 
 import org.springframework.stereotype.Service;
 
+import com.hcl.stocks.entities.TradeHistory;
+
 @Service
 public interface TradeHistoryService {
-
+	public TradeHistory confirmOrder(TradeHistory trade);
 }
